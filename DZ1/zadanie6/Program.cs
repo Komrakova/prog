bf55e4f52262a4475c2150c3405982622a7f0422
Комракова Ирина <baby-sale@mail.ru>
2022-09-06 15:32:52 +0300

    zadanie6

--- DZ1/zadanie6/Program.cs
+++ DZ1/zadanie6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+int numb = Convert.ToInt32(Console.ReadLine());
+int ostatok = numb % 2;
+
+if(ostatok == 0) 
+{
+   Console.WriteLine("Ваше число четное");
+}
+else
+{
+    Console.WriteLine("Ваше число нечетное");
+}
