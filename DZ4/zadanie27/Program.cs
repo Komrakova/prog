2e6cf439c65a2c8fe83c282a61e8119da63b2373
Комракова Ирина <baby-sale@mail.ru>
2022-09-15 15:37:45 +0300

    zadanie27

---
+++ DZ4/zadanie27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+numb = Math.Abs(numb);
+int ostatok = numb;
+while(ostatok != 0)
+{
+    ostatok = numb % 10;
+    sum = sum + ostatok;
+    numb = numb / 10;
+
+} 
+Console.WriteLine($"Сумма всех цифр в числе = {sum}");
+ 
