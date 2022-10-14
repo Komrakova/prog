4ea5144ef8af03aea9d3f051eea9e45216363872
Комракова Ирина <baby-sale@mail.ru>
2022-10-14 20:22:05 +0300

    zadanie60

---
+++ DZ8/zadanie60/Program.cs
@@ -0,0 +1,101 @@
+﻿//  Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+Console.WriteLine("задайте размерность трехмерного массива");
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+int K = Convert.ToInt32(Console.ReadLine());
+
+int[,,] array3D = new int[M, N, K];
+int[] numb = new int[M * N * K];
+int inumb = 0;
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        for (int k = 0; k < K; k++)
+        {
+            array3D[i,j,k] = new Random().Next(10, 100);
+            numb[inumb] = array3D[i,j,k];
+            for (int l = 0; l < M * N * K; l++)
+            {
+                if (array3D[i,j,k] == numb[inumb]) 
+                {
+                    array3D[i,j,k] = array3D[i,j,k] + 1;
+                }
+            }
+            Console.Write($" {array3D[i,j,k]} ({i+1}, {j+1}, {k+1})");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+
+// CreateArray(array3D);
+// WriteArray(array3D);
+
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
+
+// void WriteArray (int[,,] array3D)
+// {
+//   for (int i = 0; i < array3D.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array3D.GetLength(1); j++)
+//     {
+//       Console.Write($"X({i}) Y({j}) ");
+//       for (int k = 0; k < array3D.GetLength(2); k++)
+//       {
+//         Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+//       }
+//       Console.WriteLine();
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+// void CreateArray(int[,,] array3D)
+// {
+//   int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+//   int  number;
+//   for (int i = 0; i < temp.GetLength(0); i++)
+//   {
+//     temp[i] = new Random().Next(10, 100);
+//     number = temp[i];
+//     if (i >= 1)
+//     {
+//       for (int j = 0; j < i; j++)
+//       {
+//         while (temp[i] == temp[j])
+//         {
+//           temp[i] = new Random().Next(10, 100);
+//           j = 0;
+//           number = temp[i];
+//         }
+//           number = temp[i];
+//       }
+//     }
+//   }
+//   int count = 0; 
+//   for (int x = 0; x < array3D.GetLength(0); x++)
+//   {
+//     for (int y = 0; y < array3D.GetLength(1); y++)
+//     {
+//       for (int z = 0; z < array3D.GetLength(2); z++)
+//       {
+//         array3D[x, y, z] = temp[count];
+//         count++;
+//       }
+//     }
+//   }
+// }
+
